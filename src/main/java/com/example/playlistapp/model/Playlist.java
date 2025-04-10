1ca58b22ea932d51ee5155df5026607864e5cf68
user1hunter <user1hunter@gmail.com>
2025-04-10 11:23:43 -0500

    Initial Setup of Models

--- src/main/java/com/example/playlistapp/model/Playlist.java
+++ src/main/java/com/example/playlistapp/model/Playlist.java
@@ -1,5 +1,24 @@
 package com.example.playlistapp.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Playlist {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  private String description;
+
+  // Optional: associate with user
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User owner;
 }
--- src/main/java/com/example/playlistapp/model/User.java
+++ src/main/java/com/example/playlistapp/model/User.java
@@ -1,5 +1,32 @@
 package com.example.playlistapp.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users") // 'user' is a reserved word in some SQL dialects
 public class User {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(unique = true)
+  private String username;
+
+  private String password;
+
+  @ElementCollection(fetch = FetchType.EAGER)
+  @Enumerated(EnumType.STRING)
+  private Set<Role> roles = new HashSet<>();
+
+  // Optional: playlists owned by the user
+  @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
+  private Set<Playlist> playlists = new HashSet<>();
 }
